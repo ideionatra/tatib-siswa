970810eb8134efc332d66476e661363779524a9a
ideionatra <dioputra1423@gmail.com>
2026-06-24 16:56:14 +0800

    feat: create sidebar with link and logo

---
+++ components/sidebar.php
@@ -0,0 +1,61 @@
+<div class="sidebar-container">
+    <div class="sidebar-title">
+        <img src="../public/assets/images/logo_tatib.png" alt="">
+        <div class="sidebar-title-text">
+            <h1>
+                TATIB SISWA
+            </h1>
+            <div>
+                <p>SMA BISNIS DAN</p>
+                <p>TEKNOLOGI INDONESIA</p>
+            </div>
+        </div>
+    </div>
+    <div class="sidebar-lower">
+        <div>
+            <a href="/" class="sidebar-list">
+                <div>
+                    <span class="material-icons">
+                        home
+                    </span>
+                </div>
+                <p>Home</p>
+            </a>
+            <a href="/class-list" class="sidebar-list">
+                <div>
+                    <span class="material-icons">
+                        school
+                    </span>
+                </div>
+                <p>Daftar Kelas</p>
+            </a>
+            <a href="/laporan" class="sidebar-list">
+                <div>
+                    <span class="material-icons">
+                        assignment_add
+                    </span>
+                </div>
+                <p>Laporan</p>
+            </a>
+            <a href="/data-pelanggaran" class="sidebar-list">
+                <div>
+                    <span class="material-icons">
+                        fmd_bad
+                    </span>
+                </div>
+                <p>Data Pelanggaran</p>
+            </a>
+            <a href="/data-prestasi" class="sidebar-list">
+                <div>
+                    <span class="material-icons">
+                        gpp_good
+                    </span>
+                </div>
+                <p>Data Prestasi</p>
+            </a>
+        </div>
+        <div class="copyright">
+            All Rights Resevred &copy; GokGokGok 2026
+        </div>
+    </div>
+</div>
